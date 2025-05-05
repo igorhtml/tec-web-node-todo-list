80e1974dfe0ab87895bd3b57b562402d7c5e1f90
Igor Cesar <98989076+igorhtml@users.noreply.github.com>
2025-05-05 14:36:18 -0300

    Deletar arquivos temporarios

--- teste.js
+++
@@ -1,5 +0,0 @@
-tarefas = ["tarefa 1 | Status: [ ]", "tarefa 2 | Status: [ ]", "tarefa 3 | Status: [ ]"];
-
-if(tarefas[0].includes("Status: [ ]")){
-    console.log("Sim");
-} else console.log("Não");
